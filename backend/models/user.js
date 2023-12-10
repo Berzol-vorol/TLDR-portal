46e3aa49fd8da5aa3151d08b66b4eb0ccc5720e8
Nalexx6 <nikita2002oleksienko@gmail.com>
2023-12-10 19:07:02 +0200

    deprecated image fix

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     login: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
-    image: { type: String, required: true },
+    image: { type: String, required: false },
     rating: { type: Number, required: true },
     summaries: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Summary'}],
     reviews: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Review'}],
