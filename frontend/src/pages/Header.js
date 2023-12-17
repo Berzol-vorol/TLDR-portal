98ed799f845ee5a884ec937c066edae471e7f2a6
Eugen Vorobiov <jeka.vorobjov@gmail.com>
2023-12-17 21:59:55 +0200

    routing fix

--- frontend/src/pages/Header.js
+++ frontend/src/pages/Header.js
@@ -1,28 +1,29 @@
-import React, {useContext, useState,  useEffect} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import "./Header.css"
-import { useNavigate } from "react-router-dom";
-import { UserContext } from "../context/UserContext";
+import {Link, useNavigate} from "react-router-dom";
+import {UserContext} from "../context/UserContext";
 
 const Header = () => {
-    const { user } = useContext(UserContext);
+    const {user} = useContext(UserContext);
     const [isLoggedIn, setIsLoggedIn] = useState(null);
 
     let navigate = useNavigate();
 
     useEffect(() => {
-        if (user) setIsLoggedIn(true);
-        else setIsLoggedIn(false);
-    }, [user]
+            if (user) setIsLoggedIn(true);
+            else setIsLoggedIn(false);
+        }, [user]
     );
-    return(
+    return (
         <div className="future-header">
             <div className="header-content">
                 <div className="header-left-block">
-                    <div className="company-name" onClick={() => navigate("/")}>TLDR</div>
+                    <Link className="company-name" to={("/")}>TLDR</Link>
                 </div>
                 <div className="header-right-block">
-                    { isLoggedIn ? <div className="company-name" onClick={() => navigate("/profile")}>Hi, {user.login}!</div> :
-                        <div className="company-name" onClick={() => navigate("/sign_in")}>Log in</div>
+                    {isLoggedIn ?
+                        <Link  className="company-name" to={"/profile"}>Hi, {user.login}!</Link> :
+                        <Link className="company-name" to={"/sign_in"}>Log in</Link>
                     }
                 </div>
             </div>
